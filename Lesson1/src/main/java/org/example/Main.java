0704205548c683148b23c0e40199e9dfa4820ea5
Nortin Powers <a.nortin@gmail.com>
2022-10-20 18:08:31 +0300

    Added a homework solution for those who are bored

---
+++ Lesson1/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a some integers (sample: \"integer + space + integer..\") and press Enter:");
+        String st = sc.nextLine();
+        Solution solution = new Solution();
+        if (solution.numberOfInt(st) == 0) {
+            System.out.println("There are no positive integers among the data");
+        }
+        if (solution.numberOfInt(st) > 0) {
+            System.out.println("The count of positive integers is " + solution.numberOfInt(st));
+        }
+    }
+    static class Solution {
+        public int numberOfInt(String s) {
+            int count = 0;
+            try {
+                String[] sts = s.split(" ");
+                for (String res : sts) {
+                    if (Integer.parseInt(res) > 0) {
+                        count++;
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Check the entered data");
+                return -1;
+            }
+            return count;
+        }
+    }
+}
