6cb800b515e397d9392ba24ff0fdc481db8b8963
Nortin Powers <a.nortin@gmail.com>
2022-10-20 18:14:28 +0300

    Renamed the task

--- Lesson1/src/main/java/org/example/Task2.java
+++ Lesson1/src/main/java/org/example/Task2.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a some integers (sample: \"integer + space + integer..\") and press Enter:");
