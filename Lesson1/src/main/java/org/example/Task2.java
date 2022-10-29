c188ac5ab67fa200f2e3fa3eead3caf0fbd1f852
Nortin Powers <a.nortin@gmail.com>
2022-10-29 17:15:14 +0300

    реализовал через дефолтный интерфейс, реализовал возможность ввода вещественных чисел, убрал лишние ошибки и авто завершение при невозможности запарсить.

--- Lesson1/src/main/java/org/example/Task2.java
+++ Lesson1/src/main/java/org/example/Task2.java
@@ -2,38 +2,33 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Task2 {
+public class Task2 implements NumberOfPositiveNumbers_able {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a some integers (pattern: \"integer + space + integer..\") and press Enter:");
         String st = sc.nextLine();
-        Solution solution = new Solution();
-        try {
-            int count = solution.parseArgsAndGetCountValue(st);
-            if (count > 0) {
-                System.out.println("The count of positive integers is " + count);
-            } else {
-                System.out.println("There are no positive integers among the data");
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println("Invalid input. Check the entered data");
+        Task2 task2 = new Task2();
+        int count = task2.parseArgsAndGetCountValue(st);
+        if (count > 0) {
+            System.out.println("The count of positive integers is " + count);
+        } else {
+            System.out.println("There are no positive integers among the data");
         }
     }
+}
 
-    static class Solution {
-        public int parseArgsAndGetCountValue(String s) {
-            int count = 0;
+interface NumberOfPositiveNumbers_able {
+    default int parseArgsAndGetCountValue(String s) {
+        int count = 0;
+        String[] sts = s.replace(",", ".").split(" ");
+        for (String res : sts) {
             try {
-                String[] sts = s.split(" ");
-                for (String res : sts) {
-                    if (Integer.parseInt(res) > 0) {
-                        count++;
-                    }
+                if (Double.parseDouble(res) > 0) {
+                    count++;
                 }
             } catch (Exception e) {
-                throw new IllegalArgumentException();
             }
-            return count;
         }
+        return count;
     }
 }
