a88c416830a8875489cac4e70ae0260bc6956ff1
Nortin Powers <a.nortin@gmail.com>
2022-11-01 00:58:31 +0300

    решение домашнего задания + попробовал использовать тесты

---
+++ Lesson3/src/main/java/org/example/Homework.java
@@ -0,0 +1,93 @@
+package org.example;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Homework {
+    public static void main(String[] args) {
+        //Некоторые тесты для проверки задач.
+        System.out.println(sum(100, 200));
+        System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        System.out.println(max(56, 349));
+        System.out.println(average(new int[]{0, -2, 3, -1, 5}));
+        System.out.println(max(new int[]{1, 2, 3, 4, 5, 100, 99}));
+        System.out.println(calculateHypotenuse(3, 4));
+    }
+
+    /**
+     * 1. Метод должен вернуть сумму двух чисел a и b
+     * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
+     **/
+    public static int sum(int a, int b) {
+        BigInteger aValue = new BigInteger(String.valueOf(a));
+        BigInteger result = aValue.add(BigInteger.valueOf(b));
+        if (result.compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE))) > 0) {
+            return -1;
+        }
+        return result.intValue();
+    }
+
+    /**
+     * Метод должен вернуть максимальное значение из двух чисел
+     *
+     * <p>
+     * Example1:
+     * a = 4,
+     * b = 5
+     * <p>
+     * Метод должен вернуть 5
+     * Example2:
+     * a = 10,
+     * b = 10
+     * <p>
+     * Метод должен вернуть 10
+     */
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    /**
+     * Метод должен вернуть среднее значение из массива чисел
+     * (необходимо сумму всех элеменов массива разделить на длину массива)
+     * <p>
+     * Example:
+     * array = {1,2,3,4,5}
+     * Метод должен return 3.0
+     */
+    public static double average(int[] array) {
+        double sumArrayElements = 0;
+        for (int element : array) {
+            sumArrayElements = sumArrayElements + element;
+        }
+        return sumArrayElements / array.length;
+    }
+
+    /**
+     * Метод должен вернуть максимальый элемент массива. Пример: array = {1,2,10,3} метод возвращает 10
+     **/
+    public static int max(int[] array) {
+        Arrays.sort(array);
+        if (array.length > 0) {
+            return array[array.length - 1];
+        } else {
+            throw new ArrayIndexOutOfBoundsException("Array is empty");
+        }
+    }
+
+    /**
+     * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
+     * <p>
+     * Example1:
+     * 3
+     * 4
+     * return 5
+     * <p>
+     * Example2:
+     * 12
+     * 16
+     * return 20
+     */
+    public static double calculateHypotenuse(int a, int b) {
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+}
---
+++ Lesson3/src/test/java/org/example/HomeworkTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class HomeworkTest extends TestCase {
+
+    public void testSumElements() {
+        Assert.assertEquals(300, Homework.sum(100, 200));
+        Assert.assertEquals(-1, Homework.sum(Integer.MAX_VALUE, 1));
+        Assert.assertEquals(-1, Homework.sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+    }
+
+    public void testMaxElement() {
+        Assert.assertEquals(5, Homework.max(4, 5));
+        Assert.assertEquals(10, Homework.max(10, 10));
+        Assert.assertEquals(349, Homework.max(56, 349));
+    }
+
+    public void testAverageArrayValue() {
+        Assert.assertEquals(3.0, Homework.average(new int[]{1, 2, 3, 4, 5}), 0.0);
+        Assert.assertEquals(1.0, Homework.average(new int[]{0, -2, 3, -1, 5}), 0.0);
+    }
+
+    public void testMaxArrayElement() {
+        Assert.assertEquals(10, Homework.max(new int[]{1, 2, 10, 3}));
+        Assert.assertThrows(ArrayIndexOutOfBoundsException.class, () ->
+                Homework.max(new int[]{})
+        );
+        Assert.assertEquals(100, Homework.max(new int[]{1, 2, 3, 4, 5, 100, 99}));
+
+    }
+
+    public void testCalculateHypotenuse() {
+        Assert.assertEquals(5.0, Homework.calculateHypotenuse(3, 4), 0.0);
+        Assert.assertEquals(20.0, Homework.calculateHypotenuse(12, 16), 0.0);
+        Assert.assertEquals(4.472, Homework.calculateHypotenuse(4, 2), 0.001);
+    }
+}
