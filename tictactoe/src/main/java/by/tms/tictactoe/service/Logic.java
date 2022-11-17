1f519806d4e4c9b6b13695d471226ee484c3a875
Nortin Powers <a.nortin@gmail.com>
2022-11-17 16:21:46 +0300

    realized methods isWinnerX and isWinnerO

--- tictactoe/src/main/java/by/tms/tictactoe/service/Logic.java
+++ tictactoe/src/main/java/by/tms/tictactoe/service/Logic.java
@@ -3,7 +3,6 @@ package by.tms.tictactoe.service;
 import by.tms.tictactoe.model.Figure;
 
 public class Logic {
-
     private final Figure[][] table;
 
     public Logic(Figure[][] table) {
@@ -11,13 +10,41 @@ public class Logic {
     }
 
     public boolean isWinnerX() {
+        for (int i = 0, j = 0; i < table.length; i++) {
+            if (table[i][j].hasMarkX() && table[i][j + 1].hasMarkX() && table[i][j + 2].hasMarkX()) {
+                return true;
+            }
+        }
+        for (int i = 0, j = 0; j < table.length; j++) {
+            if (table[i][j].hasMarkX() && table[i + 1][j].hasMarkX() && table[i + 2][j].hasMarkX()) {
+                return true;
+            }
+        }
         //реализовать логику по проверке, что 3 крестика в одной строке или столбце или по диагонали
-        return false;
+        if (table[0][0].hasMarkX() && table[1][1].hasMarkX() && table[2][2].hasMarkX()) {
+            return true;
+        } else {
+            return table[0][2].hasMarkX() && table[1][1].hasMarkX() && table[2][0].hasMarkX();
+        }
     }
 
     public boolean isWinnerO() {
+        for (int i = 0, j = 0; i < table.length; i++) {
+            if (table[i][j].hasMarkO() && table[i][j + 1].hasMarkO() && table[i][j + 2].hasMarkO()) {
+                return true;
+            }
+        }
+        for (int i = 0, j = 0; j < table.length; j++) {
+            if (table[i][j].hasMarkO() && table[i + 1][j].hasMarkO() && table[i + 2][j].hasMarkO()) {
+                return true;
+            }
+        }
+        if (table[0][0].hasMarkO() && table[1][1].hasMarkO() && table[2][2].hasMarkO()) {
+            return true;
+        } else {
+            return table[0][2].hasMarkO() && table[1][1].hasMarkO() && table[2][0].hasMarkO();
+        }
         //реализовать логику по проверке, что 3 нолика в одной строке или столбце или по диагонали
-        return false;
     }
 
     /**
