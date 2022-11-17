28939ea24e6cb2e77fdb2c7c33612ef634e7ff50
Nortin Powers <a.nortin@gmail.com>
2022-11-17 17:28:30 +0300

    delete unnecessary comments

--- Lesson7/src/main/java/by/tms/Main.java
+++ Lesson7/src/main/java/by/tms/Main.java
@@ -71,41 +71,4 @@ public class Main {
         return list.size() > 0;
     }
 
-//    /**
-//     * Method the method creates a new person from console
-//     */
-//    public static Person createNewPerson() {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter persons name:");
-//        String name = scanner.nextLine();
-//        System.out.println("Enter persons age:");
-//        int age = getAgeFromScanner(scanner);
-//        scanner = new Scanner(System.in);
-//        System.out.println("Enter persons gender:");
-//        String gender = scanner.nextLine().toLowerCase();
-//        if (!gender.equals(Constants.MEN) && !gender.equals(Constants.WOMEN)) {
-//            System.out.println("You need to enter the gender of the person correctly (men / women)");
-//            scanner.nextLine();
-//        }
-//        System.out.println("Enter the person's country of residence:");
-//        String country = scanner.next();
-//        System.out.println("enter the person's city of residence");
-//        String city = scanner.next();
-//        return new Person(name, age, gender, new Address(country, city));
-//    }
-//    /**
-//     * Method checks the entered value of the person's age and returns the age of the person from the console
-//     */
-//    public static int getAgeFromScanner(Scanner scanner) {
-//        int age = -1;
-//        do {
-//            if (scanner.hasNextInt()) {
-//                age = scanner.nextInt();
-//            } else {
-//                System.out.println("You need to enter the age of the person correctly (0+)");
-//                scanner.nextLine();
-//            }
-//        } while (age < 0);
-//        return age;
-//    }
 }
