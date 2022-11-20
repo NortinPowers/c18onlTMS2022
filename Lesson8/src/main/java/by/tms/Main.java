7ef6df876a33116d38f3457134c8916d7a1ef32e
Nortin Powers <a.nortin@gmail.com>
2022-11-20 17:49:09 +0300

    added tack2 for lesson8

--- Lesson8/src/main/java/by/tms/Main.java
+++ Lesson8/src/main/java/by/tms/Main.java
@@ -1,7 +1,25 @@
 package by.tms;
 
+import by.tms.model.Car;
+import by.tms.model.Engine;
+import by.tms.model.FuelTank;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car pontiac = new Car(new Engine("V9"), new FuelTank(60));
+        pontiac.setBrand("Pontiac");
+        pontiac.setProductionYear(2022);
+        viewCarInfo(pontiac);
+        pontiac.getFuelTank().refuelingFuel(30);
+        pontiac.getFuelTank().refuelingFuel(35);
+        viewCarInfo(pontiac);
+        pontiac.startCar();
+        pontiac.goingCar();
+        pontiac.stopCar();
+        System.out.println(pontiac);
+    }
+
+    private static void viewCarInfo(Car car) {
+        System.out.println("Fuel level of " + car.getBrand() + " " + car.getFuelTank().getFuelLimit() + " liters");
     }
 }
