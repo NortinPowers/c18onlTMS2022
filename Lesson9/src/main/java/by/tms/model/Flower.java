60ba5197d22c80a7129a46a3c42b48813e99d44d
Nortin Powers <a.nortin@gmail.com>
2022-11-23 18:21:16 +0300

    fix of lesson9

--- Lesson9/src/main/java/by/tms/model/Flower.java
+++ Lesson9/src/main/java/by/tms/model/Flower.java
@@ -3,10 +3,12 @@ package by.tms.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class Flower {
     private static int floverCount;
     private String name;
