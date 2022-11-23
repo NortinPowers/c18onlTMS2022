60c9b2341ced8c0fbfd70a4409bb15004d11f4d3
Nortin Powers <a.nortin@gmail.com>
2022-11-23 11:19:51 +0300

    fix utils

--- Lesson7/src/main/java/by/tms/utils/Constants.java
+++ Lesson7/src/main/java/by/tms/utils/Constants.java
@@ -3,4 +3,7 @@ package by.tms.utils;
 public final class Constants {
     public static final String MEN = "men";
     public static final String WOMEN = "women";
+
+    private Constants() {
+    }
 }
