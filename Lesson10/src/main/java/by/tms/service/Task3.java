e2119d2ce84ac0ee4b94c4aae54c6a24404a206e
Nortin Powers <a.nortin@gmail.com>
2022-12-06 20:37:55 +0300

    fixed task3 of lesson10

--- Lesson10/src/main/java/by/tms/service/Task3.java
+++ Lesson10/src/main/java/by/tms/service/Task3.java
@@ -1,17 +1,20 @@
 package by.tms.service;
 
+import by.tms.interfaces.IStart;
 import by.tms.model.Spaceport;
 import by.tms.model.shuttle.Shuttle;
 import by.tms.model.shuttle.SpaceX;
 import by.tms.model.shuttle.Virgin;
 
+import java.util.List;
+
 public class Task3 {
     public static void main(String[] args) {
         Spaceport spaceport = new Spaceport();
-        System.out.println(spaceport.launch(new Shuttle()));
-        System.out.println();
-        System.out.println(spaceport.launch(new SpaceX()));
-        System.out.println();
-        System.out.println(spaceport.launch(new Virgin()));
+        List<IStart> shuttles = List.of(new Shuttle(), new SpaceX(), new Virgin());
+        for (IStart shuttle : shuttles) {
+            System.out.println(spaceport.launch(shuttle));
+            System.out.println();
+        }
     }
 }
