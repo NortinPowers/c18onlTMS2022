0a082233b922dc1334635a4ccdf350811fd4cf52
Nortin Powers <a.nortin@gmail.com>
2022-12-08 19:33:29 +0300

    fix typer @Util

--- Lesson11/src/main/java/by/tms/utils/Typer.java
+++ Lesson11/src/main/java/by/tms/utils/Typer.java
@@ -1,8 +1,10 @@
 package by.tms.utils;
 
 import by.tms.model.clothes.Clothes;
+import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NotNull;
 
+@UtilityClass
 public class Typer {
     public static void getSelectedTypeOfClothingCharacteristics(@NotNull StringBuilder result, @NotNull Clothes clothesType) {
         result.append(clothesType.getClass().getSimpleName())
