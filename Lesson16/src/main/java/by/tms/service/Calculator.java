3d815b3a7848cdc23478698f753023def5e83ad4
Nortin Powers <a.nortin@gmail.com>
2022-12-24 21:54:33 +0300

    lesson16 hotFix magic number

--- Lesson16/src/main/java/by/tms/service/Calculator.java
+++ Lesson16/src/main/java/by/tms/service/Calculator.java
@@ -5,24 +5,26 @@ import lombok.NonNull;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import static by.tms.utils.Constants.NUMBER_SCALE;
+
 public class Calculator {
     public static <N extends Number> BigDecimal sum(@NonNull N num1, @NonNull N num2) {
         return BigDecimal.valueOf(num1.doubleValue()).add(BigDecimal.valueOf(num2.doubleValue()))
-                .setScale(3, RoundingMode.CEILING);
+                .setScale(NUMBER_SCALE, RoundingMode.CEILING);
     }
 
     public static <N extends Number> BigDecimal multiply(@NonNull N num1, @NonNull N num2) {
         return BigDecimal.valueOf(num1.doubleValue()).multiply(BigDecimal.valueOf(num2.doubleValue()))
-                .setScale(3, RoundingMode.CEILING);
+                .setScale(NUMBER_SCALE, RoundingMode.CEILING);
     }
 
     public static <N extends Number> BigDecimal divide(@NonNull N num1, @NonNull N num2) {
         return (BigDecimal.valueOf(num1.doubleValue()).divide(BigDecimal.valueOf(num2.doubleValue()),
-                3, RoundingMode.CEILING));
+                NUMBER_SCALE, RoundingMode.CEILING));
     }
 
     public static <N extends Number> BigDecimal subtraction(@NonNull N num1, @NonNull N num2) {
         return BigDecimal.valueOf(num1.doubleValue()).subtract(BigDecimal.valueOf(num2.doubleValue()))
-                .setScale(3, RoundingMode.CEILING);
+                .setScale(NUMBER_SCALE, RoundingMode.CEILING);
     }
 }
---
+++ Lesson16/src/main/java/by/tms/utils/Constants.java
@@ -0,0 +1,9 @@
+package by.tms.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+    public static final int NUMBER_SCALE = 3;
+
+}
