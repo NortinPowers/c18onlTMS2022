3e56698490bd932d71488f707bb163ba9c47bc72
Nortin Powers <a.nortin@gmail.com>
2022-12-24 22:01:18 +0300

    hotFix javaDoc from Calculator

--- Lesson16/src/main/java/by/tms/service/Calculator.java
+++ Lesson16/src/main/java/by/tms/service/Calculator.java
@@ -8,21 +8,49 @@ import java.math.RoundingMode;
 import static by.tms.utils.Constants.NUMBER_SCALE;
 
 public class Calculator {
+    /**
+     * The method returns the result of sum two numbers
+     *
+     * @param num1 extends Number
+     * @param num2 extends Number
+     * @return BigDecimal
+     */
     public static <N extends Number> BigDecimal sum(@NonNull N num1, @NonNull N num2) {
         return BigDecimal.valueOf(num1.doubleValue()).add(BigDecimal.valueOf(num2.doubleValue()))
                 .setScale(NUMBER_SCALE, RoundingMode.CEILING);
     }
 
+    /**
+     * The method returns the result of multiplying two numbers
+     *
+     * @param num1 extends Number
+     * @param num2 extends Number
+     * @return BigDecimal
+     */
     public static <N extends Number> BigDecimal multiply(@NonNull N num1, @NonNull N num2) {
         return BigDecimal.valueOf(num1.doubleValue()).multiply(BigDecimal.valueOf(num2.doubleValue()))
                 .setScale(NUMBER_SCALE, RoundingMode.CEILING);
     }
 
+    /**
+     * The method returns the result of divide two numbers
+     *
+     * @param num1 extends Number
+     * @param num2 extends Number
+     * @return BigDecimal
+     */
     public static <N extends Number> BigDecimal divide(@NonNull N num1, @NonNull N num2) {
         return (BigDecimal.valueOf(num1.doubleValue()).divide(BigDecimal.valueOf(num2.doubleValue()),
                 NUMBER_SCALE, RoundingMode.CEILING));
     }
 
+    /**
+     * The method returns the result of subtracting two numbers
+     *
+     * @param num1 extends Number
+     * @param num2 extends Number
+     * @return BigDecimal
+     */
     public static <N extends Number> BigDecimal subtraction(@NonNull N num1, @NonNull N num2) {
         return BigDecimal.valueOf(num1.doubleValue()).subtract(BigDecimal.valueOf(num2.doubleValue()))
                 .setScale(NUMBER_SCALE, RoundingMode.CEILING);
