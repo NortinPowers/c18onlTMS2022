d16366c4489e1ca245f8bcdfa966a2c2dc6ceb83
Nortin Powers <a.nortin@gmail.com>
2023-01-02 21:45:37 +0300

    fixed -> replaced by tms.comparator

--- Lesson17/src/main/java/by/tms/comparator/ProductIdComparator.java
+++ Lesson17/src/main/java/by/tms/comparator/ProductIdComparator.java
@@ -1,14 +1,14 @@
-package by.tms.service;
+package by.tms.comparator;
 
 import by.tms.model.Product;
 import lombok.NonNull;
 
 import java.util.Comparator;
 
-public class ProductIdComparator implements Comparator<Product> {
+public class ProductIdComparator implements Comparator<Product<Double>> {
     @Override
     public int compare(@NonNull Product product1, @NonNull Product product2) {
-        long result = product1.getId() - (product2.getId());
+        long result = product1.getId() - product2.getId();
         if (result > 0) {
             return 1;
         } else if (result < 0) {
