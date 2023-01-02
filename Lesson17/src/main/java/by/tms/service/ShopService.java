5a58f57eeca5bf7f4247298f53c67e8ec355e6a2
Nortin Powers <a.nortin@gmail.com>
2023-01-02 23:17:18 +0300

    fixed -> used iterator, redundant logic removed

--- Lesson17/src/main/java/by/tms/service/ShopService.java
+++ Lesson17/src/main/java/by/tms/service/ShopService.java
@@ -6,7 +6,6 @@ import by.tms.utils.CollectionUtils;
 import lombok.AllArgsConstructor;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @AllArgsConstructor
@@ -15,7 +14,7 @@ public class ShopService implements ShopServiceAware {
 
     @Override
     public boolean addProduct(Product<Double> product) {
-        List<Product<Double>> productList = shop.getProducts();
+        List<Product<Double>> productList = getAllProduct();
         boolean flag = false;
         for (Product<Double> value : productList) {
             if (value.equals(product)) {
@@ -32,29 +31,23 @@ public class ShopService implements ShopServiceAware {
 
     @Override
     public ArrayList<Product<Double>> getAllProduct() {
-        return shop.getProducts();
+        return (ArrayList<Product<Double>>) shop.getProducts();
     }
 
     @Override
     public void deleteProduct(long id) {
-        List<Product<Double>> productList = shop.getProducts();
-        if (CollectionUtils.isNotEmpty(productList)) {
-            for (int i = 0; i < productList.size(); i++) {
-                if (productList.get(i).getId() == id) {
-                    productList.remove(i);
-                    break;
-                }
-            }
-        }
+        List<Product<Double>> productList = getAllProduct();
+        productList.removeIf(product -> product.getId() == id);
     }
 
     @Override
     public void changeProduct(long id, String productName, Double productPrice) {
-        List<Product<Double>> productList = shop.getProducts();
+        List<Product<Double>> productList = getAllProduct();
         if (CollectionUtils.isNotEmpty(productList)) {
-            for (int i = 0; i < productList.size(); i++) {
-                if (productList.get(i).getId() == id) {
-                    Collections.replaceAll(productList, getAllProduct().get(i), new Product<>(id, productName, productPrice));
+            for (Product<Double> product : productList) {
+                if (product.getId() == id) {
+                    product.setName(productName);
+                    product.setPrice(productPrice);
                     break;
                 }
             }
