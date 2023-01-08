71e1df72fdcb103802876d13bc45575cf7c71a59
Nortin Powers <a.nortin@gmail.com>
2023-01-08 20:27:54 +0300

    added tests for getPostfix and modifyCount

---
+++ Lesson19/src/test/java/by/tms/utils/PersonUtilsTest.java
@@ -0,0 +1,43 @@
+package by.tms.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PersonUtilsTest {
+
+    @Test
+    void getPostfix() {
+        Assertions.assertEquals("к", PersonUtils.getPostfix(1));
+        Assertions.assertEquals("ка", PersonUtils.getPostfix(2));
+        Assertions.assertEquals("ка", PersonUtils.getPostfix(3));
+        Assertions.assertEquals("ка", PersonUtils.getPostfix(4));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(5));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(6));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(7));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(8));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(9));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(9));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(10));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(11));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(12));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(13));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(14));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(15));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(16));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(17));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(18));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(19));
+        Assertions.assertEquals("ков", PersonUtils.getPostfix(20));
+    }
+
+    @Test
+    void modifyCount() {
+        Assertions.assertEquals(1, PersonUtils.modifyCount(21));
+        Assertions.assertEquals(1, PersonUtils.modifyCount(121));
+        Assertions.assertEquals(1, PersonUtils.modifyCount(1234561));
+        Assertions.assertEquals(2, PersonUtils.modifyCount(82));
+        Assertions.assertEquals(2, PersonUtils.modifyCount(45454542));
+        Assertions.assertEquals(12, PersonUtils.modifyCount(45454512));
+        Assertions.assertEquals(16, PersonUtils.modifyCount(87816));
+    }
+}
