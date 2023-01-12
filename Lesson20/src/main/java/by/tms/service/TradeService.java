4ebc7e4198f0af7953c22c3cc8a1bbb4b433c1a1
Nortin Powers <a.nortin@gmail.com>
2023-01-13 00:42:32 +0300

    fix

--- Lesson20/src/main/java/by/tms/service/TradeService.java
+++ Lesson20/src/main/java/by/tms/service/TradeService.java
@@ -2,12 +2,14 @@ package by.tms.service;
 
 import by.tms.model.Customer;
 import by.tms.model.Shop;
-import by.tms.utils.Constants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.util.concurrent.Semaphore;
+
+import static by.tms.utils.Constants.PAYMENT_TIME_FOR_PURCHASES;
+import static by.tms.utils.Constants.PROCESSING_TIME_OF_ONE_PRODUCT;
 
 @AllArgsConstructor
 @Getter
@@ -22,7 +24,7 @@ public class TradeService implements Runnable {
         try {
             while (shop.getRegisterCount() <= semaphore.availablePermits()) {
                 semaphore.acquire();
-                Thread.sleep(customer.getProducts().size() * Constants.PROCESSING_TIME_OF_ONE_PRODUCT + Constants.PAYMENT_TIME_FOR_PURCHASES);
+                Thread.sleep(customer.getProducts().size() * PROCESSING_TIME_OF_ONE_PRODUCT + PAYMENT_TIME_FOR_PURCHASES);
                 System.out.println("Customer " + customer.getID() + " purchased the following products: " + customer.getProducts());
             }
         } catch (InterruptedException e) {
