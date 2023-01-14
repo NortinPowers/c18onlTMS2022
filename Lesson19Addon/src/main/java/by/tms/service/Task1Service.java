c468a8ad1e747b7f69fa4597206568be4a821607
Nortin Powers <a.nortin@gmail.com>
2023-01-14 18:32:49 +0300

    fixed -> task1* entering any characters does not affect the output

--- Lesson19Addon/src/main/java/by/tms/service/Task1Service.java
+++ Lesson19Addon/src/main/java/by/tms/service/Task1Service.java
@@ -11,7 +11,7 @@ public class Task1Service implements UniqueIntegersListMakeable {
         String inputStr = scanner.next();
         String[] arrayInputStr = getIntegerModifyStrFromInputStr(inputStr);
         return Arrays.stream(arrayInputStr)
-                .filter(integer -> integer.matches("[0-9]"))
+                .filter(integer -> integer.matches("[0-9]+"))
                 .distinct()
                 .collect(Collectors.joining(", "));
     }
