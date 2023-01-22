a9348cb445082a0b7915f3e51e66b2394af2c0e2
Nortin Powers <a.nortin@gmail.com>
2023-01-22 20:49:34 +0300

    added solution for hw of lesson22

--- Lesson22/src/main/java/by/tms/Main.java
+++ Lesson22/src/main/java/by/tms/Main.java
@@ -1,7 +1,10 @@
 package by.tms;
 
+import static by.tms.utils.DBUtils.SCRIPT_FILE_ADDRESS;
+import static by.tms.utils.DBUtils.initScript;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        initScript(SCRIPT_FILE_ADDRESS);
     }
 }
---
+++ Lesson22/src/main/java/by/tms/utils/DBUtils.java
@@ -0,0 +1,42 @@
+package by.tms.utils;
+
+import lombok.experimental.UtilityClass;
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@UtilityClass
+public class DBUtils {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "root";
+    public static final String SCRIPT_FILE_ADDRESS = "Lesson22/src/main/resources/script.sql";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Connection exception: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void initScript(String scriptFileAddress) {
+        Connection connection = getConnection();
+        ScriptRunner runner = new ScriptRunner(connection);
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(scriptFileAddress));
+        } catch (FileNotFoundException e) {
+            System.out.println("Exc: " + e.getMessage());
+        }
+        runner.runScript(reader);
+    }
+}
