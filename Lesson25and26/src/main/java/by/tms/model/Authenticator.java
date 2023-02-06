04475955b128f68ae9f2681c995880c2065b0e2b
Nortin Powers <a.nortin@gmail.com>
2023-02-06 14:15:34 +0300

    fix

--- Lesson25and26/src/main/java/by/tms/model/Authenticator.java
+++ Lesson25and26/src/main/java/by/tms/model/Authenticator.java
@@ -1,11 +1,8 @@
 package by.tms.model;
 
-import lombok.ToString;
-
 import java.util.HashMap;
 import java.util.Map;
 
-@ToString
 public class Authenticator {
     private Map<String, String> authenticators;
 
