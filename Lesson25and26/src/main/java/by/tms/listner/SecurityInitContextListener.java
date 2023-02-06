fc2c887259d8eb05797630f0f7119722a59d731f
Nortin Powers <a.nortin@gmail.com>
2023-02-06 12:32:01 +0300

    fixed listrer - Authenticator

--- Lesson25and26/src/main/java/by/tms/listner/SecurityInitContextListener.java
+++ Lesson25and26/src/main/java/by/tms/listner/SecurityInitContextListener.java
@@ -7,13 +7,12 @@ import by.tms.service.SecurityService;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import java.util.HashMap;
 
 @WebListener
 public class SecurityInitContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        Authenticator authenticator = new Authenticator(new HashMap<>());
+        Authenticator authenticator = new Authenticator();
         authenticator.getAuthenticators().put("test", "test");
         SecurityAware security = new SecurityService(authenticator);
         sce.getServletContext().setAttribute("security", security);
--- Lesson25and26/src/main/java/by/tms/model/Authenticator.java
+++ Lesson25and26/src/main/java/by/tms/model/Authenticator.java
@@ -1,17 +1,13 @@
 package by.tms.model;
 
-import lombok.Getter;
-
 import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Objects.nonNull;
-
-@Getter
 public class Authenticator {
-    private final Map<String, String> authenticators;
+    private Map<String, String> authenticators;
 
-    public Authenticator(Map<String, String> authenticators) {
-        this.authenticators = nonNull(authenticators) ? authenticators : new HashMap<>();
+    public Map<String, String> getAuthenticators() {
+        return nonNull(authenticators) ? authenticators : new HashMap<>();
     }
 }
