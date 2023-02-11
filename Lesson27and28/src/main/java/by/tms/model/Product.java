60633bf174745023128bf7a9a53d2627125bbbb5
Nortin Powers <a.nortin@gmail.com>
2023-02-11 15:55:44 +0300

    fix

--- Lesson27and28/src/main/java/by/tms/model/Product.java
+++ Lesson27and28/src/main/java/by/tms/model/Product.java
@@ -1,6 +1,7 @@
 package by.tms.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.io.Serializable;
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class Product implements Serializable {
     private Long id;
     private String name;
