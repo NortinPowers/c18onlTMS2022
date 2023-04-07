9793a817253269fbc25086b81d527ab9284a02a5
Nortin Powers <a.nortin@gmail.com>
2023-04-07 17:11:07 +0300

    lesson37 some spring test

--- Lesson37/src/main/java/by/tms/spring/SpringConf.java
+++ Lesson37/src/main/java/by/tms/spring/SpringConf.java
@@ -3,13 +3,23 @@ package by.tms.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @ComponentScan("by.tms.model")
+@PropertySource("classpath:spring.properties")
 public class SpringConf {
+
+    final
+    Environment environment;
+
+    public SpringConf(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public String name() {
-        return "Hello";
+        return environment.getProperty("name");
     }
 }
