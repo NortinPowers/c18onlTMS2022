ebd2fc495c01998e4e46812f7aa3c7fb5945e790
Nortin Powers <a.nortin@gmail.com>
2023-04-18 19:59:26 +0300

    tested ModelAndView

--- EshopSpring/src/main/java/by/tms/controller/AccountController.java
+++ EshopSpring/src/main/java/by/tms/controller/AccountController.java
@@ -1,22 +1,22 @@
 package by.tms.controller;
 
 import by.tms.dto.OrderFullParamDto;
-import by.tms.dto.OrderWithListDto;
 import by.tms.dto.UserDto;
 import by.tms.service.OrderService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static by.tms.utils.Constants.Attributes.*;
 import static by.tms.utils.Constants.MappingPath.ACCOUNT;
 import static by.tms.utils.ControllerUtils.getOrders;
-import static by.tms.utils.ControllerUtils.getUserId;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,18 +25,44 @@ public class AccountController {
 
     private final OrderService orderService;
 
+//    @GetMapping("/account")
+//    public String account(HttpServletRequest request) {
+//        HttpSession session = request.getSession(false);
+//        UserDto userDto = (UserDto) session.getAttribute(USER_ACCESS_PERMISSION);
+//        request.getServletContext().setAttribute(USER_DTO, userDto);
+//        List<OrderFullParamDto> orders = orderService.getOrdersById(getUserId(request));
+//        if (!orders.isEmpty()) {
+//            List<OrderWithListDto> userOrder = getOrders(orders);
+//            request.getServletContext().setAttribute(USER_ORDER, userOrder);
+//        } else {
+//            request.removeAttribute(USER_ORDER);
+//        }
+//        return ACCOUNT;
+//    }
+
     @GetMapping("/account")
-    public String account(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
+    public ModelAndView account(HttpSession session) {
+//    public String account(HttpSession session, Model model) {
+//        HttpSession session = request.getSession(false);
         UserDto userDto = (UserDto) session.getAttribute(USER_ACCESS_PERMISSION);
-        request.getServletContext().setAttribute(USER_DTO, userDto);
-        List<OrderFullParamDto> orders = orderService.getOrdersById(getUserId(request));
+//        model.addAttribute(USER_DTO, userDto);
+//        request.getServletContext().setAttribute(USER_DTO, userDto);
+        List<OrderFullParamDto> orders = orderService.getOrdersById(userDto.getId());
+//        List<OrderFullParamDto> orders = orderService.getOrdersById(getUserId(request));
+        Map<String, Object> models = new HashMap<>();
+        models.put(USER_DTO, userDto);
         if (!orders.isEmpty()) {
-            List<OrderWithListDto> userOrder = getOrders(orders);
-            request.getServletContext().setAttribute(USER_ORDER, userOrder);
-        } else {
-            request.removeAttribute(USER_ORDER);
+//            List<OrderWithListDto> userOrder = getOrders(orders);
+//            model.addAttribute(USER_ORDER, userOrder);
+            models.put(USER_ORDER, getOrders(orders));
+//            models.put(USER_ORDER, userOrder);
+//            request.getServletContext().setAttribute(USER_ORDER, userOrder);
         }
-        return ACCOUNT;
+//        else {
+//            model.addAttribute(USER_ORDER, null);
+////            request.removeAttribute(USER_ORDER);
+//        }
+        return new ModelAndView(ACCOUNT, "models", models);
+//        return ACCOUNT;
     }
 }
